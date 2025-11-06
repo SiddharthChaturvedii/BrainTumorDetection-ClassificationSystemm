03584d4c91709bde104dd28c928ee56d797d3809
SiddharthChaturvedii <chaturvedisiddharth008@gmail.com>
2025-11-06 23:16:21 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import cv2
+from keras.models import load_model
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+model = load_model('BrainTumor10Epochs.h5')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+def get_className(classNo):
+    if classNo[0] == 0:
+        return "Brain Tumor Not Found"
+    elif classNo[0] == 1:
+        return "Brain Tumor Found"
+
+def getResult(img):
+    image = cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((64, 64))
+    image = np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result = model.predict(input_img)
+    return np.argmax(result, axis=1)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def upload():
+    f = request.files['file']
+    basepath = os.path.dirname(__file__)
+    file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+    f.save(file_path)
+
+    value = getResult(file_path)
+    result = get_className(value)
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
